da3e4e59f917107250771eb4167c51e846f4e009
Shreyas Kapur <shreyaskapur@yahoo.com>
2019-04-15 22:01:08 -0400

    Add master and slave modes to triggers.

--- pishot.py
+++ pishot.py
@@ -4,25 +4,36 @@ import os
 import subprocess
 import time
 import signal
-import cv2
+import argparse
+import RPi.GPIO as GPIO
 
-import numpy as np
+INPUT_PIN = 17
+CLONE_PIN = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(CLONE_PIN, GPIO.OUT)
 
 raspivid_process = None
 trigger_process = None
+
 
 def write_frex_registers():
     process = subprocess.Popen("./i2cwrite /dev/i2c-0 3002 ff".split())
     process.wait()
 
-    process = subprocess.Popen("./i2cwrite /dev/i2c-0 3b01 00 08 00 ff ff 14 1d".split())
+    process = subprocess.Popen(
+        "./i2cwrite /dev/i2c-0 3b01 00 08 00 ff ff 14 1d".split()
+    )
     process.wait()
+
 
 def set_frex_trigger(on):
     val = "01" if on else "00"
 
     process = subprocess.Popen(("./i2cwrite /dev/i2c-0 3b08 %s" % val).split())
     process.wait()
+
 
 def open_shutter():
     global raspivid_process
@@ -31,9 +42,12 @@ def open_shutter():
     if os.path.exists("temp.264"):
         os.remove("temp.264")
 
-    raspivid_process = subprocess.Popen("raspivid -md 2 -fps 1 -pts test.pts -t 800000 -o temp.264".split())
+    raspivid_process = subprocess.Popen(
+        "raspivid -md 2 -fps 1 -pts test.pts -t 800000 -o temp.264".split()
+    )
     time.sleep(1)
     write_frex_registers()
+
 
 def close_shutter(filename):
     global raspivid_process
@@ -45,7 +59,67 @@ def close_shutter(filename):
 
     os.kill(raspivid_process.pid, signal.SIGTERM)
 
-if __name__ == "__main__":
+    print("Shutter closed!")
+
+
+def one_shot(t):
     open_shutter()
-    time.sleep(10)
+    time.sleep(t)
     close_shutter("test.png")
+
+
+def slave_loop():
+    while True:
+        GPIO.wait_for_edge(INPUT_PIN, GPIO.RISING)
+        GPIO.output(CLONE_PIN, 1)
+        open_shutter()
+        GPIO.wait_for_edge(INPUT_PIN, GPIO.FALLING)
+        GPIO.output(CLONE_PIN, 0)
+        close_shutter("test.png")
+
+
+def master_loop():
+    while True:
+        command = raw_input(">> ")
+
+        if command == "o" or command == "open":
+            GPIO.output(CLONE_PIN, 1)
+            open_shutter()
+
+        if command == "c" or command == "close":
+            GPIO.output(CLONE_PIN, 0)
+            close_shutter("test.png")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PiShot main script.")
+
+    parser.add_argument(
+        "--one", default=False, action="store_true", help="Take a single shot"
+    )
+
+    parser.add_argument(
+        "-t",
+        help="The exposure time for --one mode.",
+        action="store",
+        dest="t",
+        type=int,
+        default=10,
+    )
+
+    parser.add_argument(
+        "--slave", default=False, action="store_true", help="Run daemon in Slave mode."
+    )
+
+    parser.add_argument(
+        "--master", default=True, action="store_true", help="Run daemon in Master mode."
+    )
+
+    args = parser.parse_args()
+
+    if args.one:
+        one_shot(args.t)
+    elif args.slave:
+        slave_loop()
+    elif args.master:
+        master_loop()
